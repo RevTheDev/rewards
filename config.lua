cc1fb581eb0a63e50bead290f186a8c3e4dbbd1d
RevTheDev <45327031+RevTheDev@users.noreply.github.com>
2021-06-18 00:11:20 +0100

    patches
         "Fixed some spelling issuses",
         "Hopefully fixed spam click issue",
         "Made it so that you can disable getting weapons"
      ]

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ local type = {money=1,item=2,weapon=3} -- no touchey, thank you
 Config = {}
 
 Config.Command = false  -- Change command to true to switch to /daily
-Config.EveryBody = false -- Cvhange to false to stop using Daily Rewards for everyone
+Config.EveryBody = false -- Change to false to stop using Daily Rewards for everyone
 Config.random_rewards_enabled = true -- Set to False to Disable Random_Rewards
 Config.rblip_enabled = true -- change to false to stop using Markers/blips
 Config.AcePerms = true -- Set to true to use esx Perms E.G only superadmin can access it
@@ -35,7 +35,7 @@ Config.rewards = {
 
 Config.random_rewards = {
     {
-        chance = 90, -- this can be any whole number (higher = better chance)
+        chance = 45, -- this can be any whole number (higher = better chance)
         {
             type = type.money,
             value = 1000
@@ -50,11 +50,24 @@ Config.random_rewards = {
         }
     },
     {
-        chance = 10,
+        chance = 35,
         {
             type = type.item,
             item = "bread",
             count = 5
+        },
+        {
+            type = type.item,
+            item = "water",
+            count = 5
+        }
+    },
+	{
+        chance = 0, -- To enable Change this from 0 to any high number (higher = better chance)
+        {
+            type = type.weapon,
+            weapon = "WEAPON_PISTOL", -- if they already have the weapon, they'll only get the ammo
+            ammo = 100
         },
         {
             type = type.weapon,
